eedd8d11a4046f28b962193349b34e01a0b1c0d1
Soyuz11 <jesley11martins@gmail.com>
2024-03-10 20:53:41 -0300

    V2.0

--- app/src/main/java/com/example/projectalice/MainActivity.java
+++ app/src/main/java/com/example/projectalice/MainActivity.java
@@ -13,6 +13,8 @@ import java.io.InputStream;
 import android.util.Log;
 import android.graphics.Color;
 
+import org.json.JSONObject;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -31,15 +33,73 @@ public class MainActivity extends AppCompatActivity {
         // Define o layout da atividade
         setContentView(R.layout.activity_main);
 
-        // Encontra o botão pelo ID definido no XML
-        Button botaoRele1 = findViewById(R.id.botaoRele1);
+        // Encontra os botões pelo ID definido no XML e adiciona-os à matriz
+        Button[] arrayBotoes = new Button[8];
+        arrayBotoes[0] = findViewById(R.id.botaoRele1);
+        arrayBotoes[1] = findViewById(R.id.botaoRele2);
+        arrayBotoes[2] = findViewById(R.id.botaoRele3);
+        arrayBotoes[3] = findViewById(R.id.botaoRele4);
+        arrayBotoes[4] = findViewById(R.id.botaoRele5);
+        arrayBotoes[5] = findViewById(R.id.botaoRele6);
+        arrayBotoes[6] = findViewById(R.id.botaoRele7);
+        arrayBotoes[7] = findViewById(R.id.botaoRele8);
+
+        atualizarEstadoBotao(arrayBotoes);
 
         // Define um listener de clique para o botão
-        botaoRele1.setOnClickListener(v -> {
-            // Mostra um toast indicando que o rele 1 foi acionado
-            //Toast.makeText(getApplicationContext(), "Rele 1 Acionado!", Toast.LENGTH_SHORT).show();
-            // Faz uma solicitação HTTP para o ESP32 para acionar o rele2
-            fazerRequestHttp("rele1", botaoRele1);
+        arrayBotoes[0].setOnClickListener(v -> {
+            // Mostra um toast indicando que o rele foi acionado
+            Toast.makeText(getApplicationContext(), "Rele Acionado!", Toast.LENGTH_SHORT).show();
+            fazerRequestHttp("rele1", arrayBotoes[0]);
+        });
+
+        // Define um listener de clique para o botão
+        arrayBotoes[1].setOnClickListener(v -> {
+            // Mostra um toast indicando que o rele foi acionado
+            Toast.makeText(getApplicationContext(), "Rele Acionado!", Toast.LENGTH_SHORT).show();
+            fazerRequestHttp("rele2", arrayBotoes[1]);
+        });
+
+        // Define um listener de clique para o botão
+        arrayBotoes[2].setOnClickListener(v -> {
+            // Mostra um toast indicando que o rele foi acionado
+            Toast.makeText(getApplicationContext(), "Rele Acionado!", Toast.LENGTH_SHORT).show();
+            fazerRequestHttp("rele3", arrayBotoes[2]);
+        });
+
+        // Define um listener de clique para o botão
+        arrayBotoes[3].setOnClickListener(v -> {
+            // Mostra um toast indicando que o rele foi acionado
+            Toast.makeText(getApplicationContext(), "Rele Acionado!", Toast.LENGTH_SHORT).show();
+            fazerRequestHttp("rele4", arrayBotoes[3]);
+        });
+
+        // Define um listener de clique para o botão
+        arrayBotoes[4].setOnClickListener(v -> {
+            // Mostra um toast indicando que o rele foi acionado
+            Toast.makeText(getApplicationContext(), "Rele Acionado!", Toast.LENGTH_SHORT).show();
+            fazerRequestHttp("rele5", arrayBotoes[4]);
+        });
+
+        // Define um listener de clique para o botão
+        arrayBotoes[5].setOnClickListener(v -> {
+            // Mostra um toast indicando que o rele foi acionado
+            Toast.makeText(getApplicationContext(), "Rele Acionado!", Toast.LENGTH_SHORT).show();
+            fazerRequestHttp("rele6", arrayBotoes[5]);
+        });
+
+        // Define um listener de clique para o botão
+        arrayBotoes[6].setOnClickListener(v -> {
+            // Mostra um toast indicando que o rele foi acionado
+            Toast.makeText(getApplicationContext(), "Rele Acionado!", Toast.LENGTH_SHORT).show();
+            fazerRequestHttp("rele7", arrayBotoes[6]);
+        });
+
+        // Define um listener de clique para o botão
+        arrayBotoes[7].setOnClickListener(v -> {
+            // Mostra um toast indicando que o rele foi acionado
+            Toast.makeText(getApplicationContext(), "Rele Acionado!", Toast.LENGTH_SHORT).show();
+            fazerRequestHttp("rele8", arrayBotoes[7]);
         });
 
         // Configura o aplicativo para preencher a área de conteúdo após o EdgeToEdge
@@ -76,7 +136,7 @@ public class MainActivity extends AppCompatActivity {
 
                         // Atualiza a cor do botão com base no estado do relé
                         if (result.equals("Rele Ligado")) {
-                            botaoRele.setBackgroundColor(Color.YELLOW);
+                            botaoRele.setBackgroundColor(Color.rgb(252, 199,50));
                         } else if (result.equals("Rele Desligado")) {
                             botaoRele.setBackgroundColor(Color.GRAY);
                         }
@@ -103,5 +163,52 @@ public class MainActivity extends AppCompatActivity {
             result.append(line);
         }
         return result.toString();
+    }
+
+    // Método para atualizar a cor do botão com base no estado do relé
+    private void atualizarEstadoBotao(final Button[] arrayBotoes) {
+        // Faz uma solicitação HTTP para obter o JSON de estado dos relés
+        new Thread(() -> {
+            try {
+                URL url = new URL(ESP32_URL + "estado_reles.json");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    InputStream in = urlConnection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+
+                    // Analise o JSON para obter os estados dos relés
+                    JSONObject json = new JSONObject(response.toString());
+
+                    // Atualiza os estado dos 8 botões
+                    boolean estadoRele;
+                    for (int i = 1; i <= 8; i++) {
+                        estadoRele = json.getBoolean("Rele" + i);
+                        atualizarCorBotaoInicial(arrayBotoes[i - 1], estadoRele);
+                        Log.d("MainActivity", "Estado do Rele " + i + ": " + estadoRele);
+                    }
+
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (Exception e) {
+                Log.e("MainActivity", "Erro ao atualizar estado do botão", e);
+            }
+        }).start();
+    }
+
+    // Método para atualizar a cor do botão com base no estado do relé
+    private void atualizarCorBotaoInicial(Button botao, boolean estadoRele) {
+        if (estadoRele) {
+            // Se o relé estiver ligado (true), defina a cor do botão como amarelo
+            botao.setBackgroundColor(Color.rgb(252, 199,50));
+        } else {
+            // Se o relé estiver desligado (false), defina a cor do botão como outra cor, por exemplo, cinza
+            botao.setBackgroundColor(Color.GRAY);
+        }
     }
 }

dbfbcaaa49e6280525719821aec259cdd684a2c2
Soyuz11 <jesley11martins@gmail.com>
2024-03-11 11:19:12 -0300

    V2.2

--- app/src/main/java/com/example/projectalice/MainActivity.java
+++ app/src/main/java/com/example/projectalice/MainActivity.java
@@ -13,15 +13,24 @@ import java.io.InputStream;
 import android.util.Log;
 import android.graphics.Color;
 
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import java.util.Locale;
+import java.util.ArrayList;
 import org.json.JSONObject;
 
+
+
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_SPEECH_INPUT = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,45 +55,24 @@ public class MainActivity extends AppCompatActivity {
 
         atualizarEstadoBotao(arrayBotoes);
 
-        // Define um listener de clique para o botão
-        arrayBotoes[0].setOnClickListener(v -> {
-            fazerRequestHttp("rele1", arrayBotoes[0]);
-        });
+        // Encontra botão do reconhecimento de voz
+        Button botaoVoz = findViewById(R.id.botaoRVoz);
+
+        // Define um listener de clique para os botões
+        arrayBotoes[0].setOnClickListener(v -> fazerRequestHttp("rele1", arrayBotoes[0]));
+        arrayBotoes[1].setOnClickListener(v -> fazerRequestHttp("rele2", arrayBotoes[1]));
+        arrayBotoes[2].setOnClickListener(v -> fazerRequestHttp("rele3", arrayBotoes[2]));
+        arrayBotoes[3].setOnClickListener(v -> fazerRequestHttp("rele4", arrayBotoes[3]));
+        arrayBotoes[4].setOnClickListener(v -> fazerRequestHttp("rele5", arrayBotoes[4]));
+        arrayBotoes[5].setOnClickListener(v -> fazerRequestHttp("rele6", arrayBotoes[5]));
+        arrayBotoes[6].setOnClickListener(v -> fazerRequestHttp("rele7", arrayBotoes[6]));
+
+        botaoVoz.setOnClickListener(v -> lerAudio());
+
 
         // Define um listener de clique para o botão
-        arrayBotoes[1].setOnClickListener(v -> {
-            fazerRequestHttp("rele2", arrayBotoes[1]);
-        });
+        arrayBotoes[7].setOnClickListener(v -> fazerRequestHttp("rele8", arrayBotoes[7]));
 
-        // Define um listener de clique para o botão
-        arrayBotoes[2].setOnClickListener(v -> {
-            fazerRequestHttp("rele3", arrayBotoes[2]);
-        });
-
-        // Define um listener de clique para o botão
-        arrayBotoes[3].setOnClickListener(v -> {
-            fazerRequestHttp("rele4", arrayBotoes[3]);
-        });
-
-        // Define um listener de clique para o botão
-        arrayBotoes[4].setOnClickListener(v -> {
-            fazerRequestHttp("rele5", arrayBotoes[4]);
-        });
-
-        // Define um listener de clique para o botão
-        arrayBotoes[5].setOnClickListener(v -> {
-            fazerRequestHttp("rele6", arrayBotoes[5]);
-        });
-
-        // Define um listener de clique para o botão
-        arrayBotoes[6].setOnClickListener(v -> {
-            fazerRequestHttp("rele7", arrayBotoes[6]);
-        });
-
-        // Define um listener de clique para o botão
-        arrayBotoes[7].setOnClickListener(v -> {
-            fazerRequestHttp("rele8", arrayBotoes[7]);
-        });
 
         // Configura o aplicativo para preencher a área de conteúdo após o EdgeToEdge
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -120,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
 
                         // Atualiza a cor do botão com base no estado do relé
                         if (result.equals("Rele Ligado")) {
-                            botaoRele.setBackgroundColor(Color.rgb(252, 199,50));
+                            botaoRele.setBackgroundColor(Color.rgb(252, 199, 50));
                         } else if (result.equals("Rele Desligado")) {
                             botaoRele.setBackgroundColor(Color.GRAY);
                         }
@@ -189,10 +177,49 @@ public class MainActivity extends AppCompatActivity {
     private void atualizarCorBotaoInicial(Button botao, boolean estadoRele) {
         if (estadoRele) {
             // Se o relé estiver ligado (true), defina a cor do botão como amarelo
-            botao.setBackgroundColor(Color.rgb(252, 199,50));
+            botao.setBackgroundColor(Color.rgb(252, 199, 50));
         } else {
             // Se o relé estiver desligado (false), defina a cor do botão como outra cor, por exemplo, cinza
             botao.setBackgroundColor(Color.GRAY);
+        }
+    }
+
+    // Método para ler o audio
+    private void lerAudio() {
+        // Cria um intent para o reconhecimento de voz
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        // Define o modelo de linguagem livre para reconhecimento de voz
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        // Define o idioma padrão para o reconhecimento de voz
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        // Define a mensagem exibida durante o reconhecimento de voz
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Fale algo...");
+
+        try {
+            // Inicia método para o reconhecimento de voz e espera o resultado
+            startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
+        } catch (Exception e) {
+            // Trata erros ao iniciar o reconhecimento de voz
+            Toast.makeText(this, "Erro ao iniciar reconhecimento de voz.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Verifica se o resultado é para o reconhecimento de voz
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT) {
+            // Verifica se o reconhecimento de voz foi bem-sucedido e há dados retornados
+            if (resultCode == RESULT_OK && data != null) {
+                // Obtém os resultados do reconhecimento de voz como uma lista de strings
+                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                // Obtém o texto reconhecido da primeira string na lista
+                String voiceInput = result.get(0);
+
+                // Aqui você pode usar o texto reconhecido, por exemplo, exibir em um toast
+                Toast.makeText(this, "Texto Reconhecido: " + voiceInput, Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
